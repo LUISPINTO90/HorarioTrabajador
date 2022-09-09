4846e02dcb57d415a1cb5ca797f44f9b39394665
LUISPINTO90 <lpinto1@ucol.mx>
2022-09-09 01:38:07 -0500

    Refactoring functionality at getTimeWorked() in workingTime.js

--- src/workingTime.js
+++ src/workingTime.js
@@ -15,57 +15,6 @@ export default class WorkingTime {
     this.checkOutSeconds = checkOutSeconds;
   }
 
-  hourIsValid() {
-    if (
-      (this.checkInHour && this.checkOutHour) <= 24 &&
-      (this.checkInHour && this.checkOutHour) >= 1
-    ) {
-      return true;
-    }
-
-    return false;
-  }
-  minutesAreValid() {
-    if (
-      (this.checkInMinutes && this.checkOutMinutes) <= 60 &&
-      (this.checkInMinutes && this.checkOutMinutes) >= 0
-    ) {
-      return true;
-    }
-
-    return false;
-  }
-  secondsAreValid() {
-    if (
-      (this.checkInSeconds && this.checkOutSeconds) <= 60 &&
-      (this.checkInSeconds && this.checkOutSeconds) >= 0
-    ) {
-      return true;
-    }
-
-    return false;
-  }
-
-  timeIsValid() {
-    if (this.checkInHour <= this.checkOutHour) {
-      if (this.checkInMinutes > this.checkOutMinutes) {
-        return false;
-      }
-      if (this.checkInSeconds > this.checkOutSeconds) {
-        return false;
-      }
-
-      if (
-        this.hourIsValid() &&
-        this.minutesAreValid() &&
-        this.secondsAreValid()
-      )
-        return true;
-    }
-
-    return false;
-  }
-
   getTimeWorked() {
     let workedHours = this.checkOutHour - this.checkInHour;
     let workedMinutes = this.checkOutMinutes - this.checkInMinutes;
